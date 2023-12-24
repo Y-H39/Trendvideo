df55f54d9716795e8689fa01ef317f23310844f5
Y-H39 <linemo159@gmail.com>
2023-12-24 06:29:56 +0000

    feat: 投稿機能実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+     else
+      render :new
+     end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :comment)
+  end
+end
