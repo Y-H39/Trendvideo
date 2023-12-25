19bc1cfe1b666222c9abb91aded8a49765aa850f
Y-H39 <linemo159@gmail.com>
2023-12-25 18:55:55 +0000

    feat: ランキング機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
-  default_scope -> { order(created_at: :desc) }
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
