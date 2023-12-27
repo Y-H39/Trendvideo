eb5236d3a2175da94883e594d5a61b390b4eb51f
Y-H39 <linemo159@gmail.com>
2023-12-27 11:33:57 +0000

    feat: show(user)実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :users, only: :show
   resources :posts do
     resource :favorites, only: [:create, :destroy]
   end
