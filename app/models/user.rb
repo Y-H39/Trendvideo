46650db9baa04d1e0e3b20779325afe4a8873606
Y-H39 <linemo159@gmail.com>
2023-12-28 16:16:12 +0000

    feat: devise更新(name追加)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: {maximum: 50}
 end
