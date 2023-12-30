e42a200baad74189f24129e2e29eb71732061fa9
Y-H39 <linemo159@gmail.com>
2023-12-30 20:01:04 +0000

    feat: faker実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(name:  "Example User",
+             email: "example@domain.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+20.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@domain.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+users = User.order(:created_at).take(6)
+2.times do
+  title = Faker::Lorem.sentence(word_count: 5)
+  url = "https://youtu.be/gcgKUcJKxIs?si=iIrAvDjdz5dKsZKG"
+  comment = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.posts.create!(title: title, url: url, comment: comment) }
+end
+
+11.times do |n|
+  users = User.order(:created_at).take(n+1)
+  post = Post.find(n+1)
+  users.each { |user| user.favorites.create!(user_id: user.id, post_id: post.id) }
+end
+
