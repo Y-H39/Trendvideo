89372d5e46b2acd6d6f0e08d81f4186eeb667719
Y-H39 <linemo159@gmail.com>
2024-01-20 02:19:18 +0900

    test: posts_spec更新(タグ機能)

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,10 +15,6 @@ class Post < ApplicationRecord
     Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
   end
 
-  def self.ransackable_attributes(auth_object = nil)
-    auth_object ? super : %w(title comment)
-  end
-
   private
   def url_should_start_with_youtube
     unless url&.start_with?("https://youtu.be/")
